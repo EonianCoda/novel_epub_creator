3211afe7ab162961016a46d9b232086418aa4d9e
EonianCoda <textuseful159000@gmail.com>
2022-05-03 14:59:52 +0800

    optimize app.py code

--- app.py
+++ app.py
@@ -34,85 +34,86 @@ def translate_and_convert_novel(novel_name:str):
     else:
         return True
 
-def add_option(result,temp):
-    
-    for book_name in result:
-        opt =  {
-            "type": "button",
-            "style": "link",
-            "height": "sm",
-            "action": {
-                "type": "message",
-                "label": book_name,
-                "text": book_name
-            }
-        }
-        temp['footer']['contents'].append(opt)
+def reply_text_message(event, msg:str):
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text =msg))
 
-    return 
+# def add_option(result,temp):
+#     for book_name in result:
+#         opt =  {
+#             "type": "button",
+#             "style": "link",
+#             "height": "sm",
+#             "action": {
+#                 "type": "message",
+#                 "label": book_name,
+#                 "text": book_name
+#             }
+#         }
+#         temp['footer']['contents'].append(opt)
+#     return 
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
 
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def echo(event):
     global DOWNLOADER
-    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
-        try :
-            action, data = event.message.text.split(',')
-            if action == '搜尋':
-                result = DOWNLOADER.search(data)
-                if result == None :
-                    line_bot_api.reply_message(
-                        event.reply_token,
-                        TextSendMessage(text='NONO')
-                    )
-                else:
-                    temp =  json.load(open(LINE_BOT_TEMPLATE_FILE_PATH,'r',encoding='utf-8'))
-                    temp['body']['contents'][0]['text'] = data
-                    for metadata in result:
-                        opt =  {
-                            "type": "button",
-                            "style": "link",
-                            "height": "sm",
-                            "action": {
-                                "type": "message",
-                                "label": metadata['novel_name'],
-                                "text": '下載,{}&{}&{}'.format(metadata['novel_name'], metadata['novel_idx'], metadata['source_idx'])
-                            }
+    if event.source.user_id == "Udeadbeefdeadbeefdeadbeefdeadbeef":
+        return
+
+    try:
+        action, data = event.message.text.split(',')
+        if action == '搜尋':
+            result = DOWNLOADER.search(data)
+            if result == None :
+                reply_text_message("搜尋不到此小說!!")
+            else:
+                # Create form
+                form = json.load(open(LINE_BOT_TEMPLATE_FILE_PATH, 'r',encoding='utf-8'))
+                form['body']['contents'][0]['text'] = data # Set title
+                # Add option
+                for metadata in result:
+                    opt =  {
+                        "type": "button",
+                        "style": "link",
+                        "height": "sm",
+                        "action": {
+                            "type": "message",
+                            "label": metadata['novel_name'],
+                            "text": '下載,{}&{}&{}'.format(metadata['novel_name'], metadata['novel_idx'], metadata['source_idx'])
                         }
-                        temp['footer']['contents'].append(opt)
-                    FlexMessage = temp
-                    line_bot_api.reply_message(event.reply_token,FlexSendMessage(data, FlexMessage))
-            elif action == '下載':
-                novel_name, novel_idx, source_idx = data.split('&')
-                DOWNLOADER.download(create_metadata(novel_name, novel_idx, source_idx))
-                # Success
-                if translate_and_convert_novel(novel_name):
-                    # upload file
-                    file_name = "{}.epub".format(novel_name)
-                    link = upload(filename=file_name, local_file_path=get_OUTPUT_PATH(novel_name))
-                    line_bot_api.reply_message(event.reply_token,TextSendMessage(text =link))
-                # Fail
-                else:
-                    line_bot_api.reply_message(event.reply_token,TextSendMessage(text ='下載錯誤'))
-            else :
-                message = '請先使用[搜尋,XXX]並選擇欲下載的書目'
-                line_bot_api.reply_message(event.reply_token,TextSendMessage(text =message))
-        except Exception as e:
-            print(e)
-            message = '錯囉，請先使用[搜尋,XXX]並選擇欲下載的書目'
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text =message))
-        
+                    }
+                    form['footer']['contents'].append(opt)
+                
+                FlexMessage = form
+                line_bot_api.reply_message(event.reply_token, FlexSendMessage(data, FlexMessage))
+        elif action == '下載':
+            novel_name, novel_idx, source_idx = data.split('&')
+            DOWNLOADER.download(create_metadata(novel_name, novel_idx, source_idx))
+            # Success
+            if translate_and_convert_novel(novel_name):
+                # upload file
+                file_name = "{}.epub".format(novel_name)
+                link = upload(filename=file_name, local_file_path=get_OUTPUT_PATH(novel_name))
+                reply_text_message(event, link)
+            # Fail
+            else:
+                reply_text_message(event, '下載過程發生錯誤')
+        else:
+            reply_text_message(event, '請使用正確的指令:"搜尋,XXX"，接著選擇想下載的小說')
+    except Exception as e:
+        reply_text_message(event, '發生不明錯誤!\n{}'.format(e))
+        print(e)
+    
 if __name__ == "__main__":
     app.run()
