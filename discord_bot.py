5f76741244326debd595fe0f9fe798a84af9200a
llooww10120 <llooww10120@gmail.com>
2022-05-08 00:55:21 +0800

    add mention function

--- discord_bot.py
+++ discord_bot.py
@@ -40,6 +40,7 @@ async def on_message(message):
         if result == None :
             await channel.send(mention(user.id)+"搜尋不到此小說!!")
         else:
+            #排版
             max_book_len = max([len(i['novel_name']) for i in result])
             max_src_len = max([len(WEB_NAME[i['source_idx']]) for i in result])
             formated_str = '|\t{0:\u3000<%ds}\t{1:\u3000<%ds}\t{2:\u3000<%ds}\t|' % (4,max_book_len,max_src_len)
@@ -49,19 +50,21 @@ async def on_message(message):
                                 formated_str.format('標籤','小說名稱','資料來源')+'\n'+
                                '\n'.join(i for i in name)+'\n'+
                                 border_line+'要下載哪一個？')
+            
             def check(m):
                 return   user == m.author and m.channel == channel
             msg = await client.wait_for('message',check=check)
             book = result[int(msg.content)-1]
             novel_name, novel_idx, source_idx = book['novel_name'],book['novel_idx'],book['source_idx']
+            
             book_id = database.get_download(str(('{}.epub'.format(novel_name),source_idx)))
             if book_id!= None:
                 await channel.send(link_message(user.id,novel_name,GOOGLE_DRIVE_PATH.format(book_id)))
                 return
+
             await channel.send('正在下載《{}》！'.format(novel_name))
             DOWNLOADER.download(create_metadata(novel_name, novel_idx, source_idx))
             if translate_and_convert(TMP_TXT_PATH, get_OUTPUT_PATH(novel_name)):
-                # upload file
                 file_name = "{}.epub".format(novel_name)
                 file = (file_name,source_idx)
                 novel_link = upload(file =file, local_file_path=get_OUTPUT_PATH(novel_name))
