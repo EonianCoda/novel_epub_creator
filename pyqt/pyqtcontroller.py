754ec5ff7c3d5d75df52c95fd45cbea0d9da29ec
Ming <llooww10120@gmail.com>
2022-09-10 15:40:19 +0800

    update UI

--- pyqt/pyqtcontroller.py
+++ pyqt/pyqtcontroller.py
@@ -10,16 +10,19 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.setup_control()
     def setup_control(self):
-        self.ui.pushButton.clicked.connect(self.open_file) 
-        # self.ui.folder_button.clicked.connect(self.open_folder)
-
+        self.ui.open_file_button.clicked.connect(self.open_file) 
+        # self.ui..clicked.connect(self.open_folder)
+        self.ui.start_trans.clicked.connect(self.start_trans)
     def open_file(self):
         filename, filetype = QFileDialog.getOpenFileName(self,
                   "Open file",
                   "./")                 # start path
         print(filename, filetype)
         self.ui.textBrowser.setText(filename)
-
+    def start_trans(self):
+        #todo
+        print("test")
+        pass
 
 
 
